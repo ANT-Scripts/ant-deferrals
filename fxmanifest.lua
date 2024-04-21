cb770ef3c659c333ecb73699814a379ff4c01182
SwagDaddyBrad <bradlee.kolvoord@gmail.com>
2024-04-20 21:29:02 -0400

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,8 @@ game 'common'
 server_script "server/sv_queue_config.lua"
 server_script "connectqueue.lua"
 server_script "functions.lua"
-
 server_script "shared/sh_queue.lua"
+
 client_script "shared/sh_queue.lua"
+
+server_script "server/rules_agreement.lua"
